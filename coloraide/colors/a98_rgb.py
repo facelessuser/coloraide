24a52bb1974e0086e42b4b12f471b93355d7a620
facelessuser <faceless.shop@gmail.com>
2020-09-24 18:28:29 -0600

    Forgot to commit a98 changes

--- coloraide/colors/a98_rgb.py
+++ coloraide/colors/a98_rgb.py
@@ -1,5 +1,7 @@
 """A98 RGB color class."""
 from ._rgb import RGB
+from ._space import RE_GENERIC_MATCH
+import re
 
 
 class A98_RGB(RGB):
@@ -7,6 +9,7 @@ class A98_RGB(RGB):
 
     SPACE = "a98-rgb"
     DEF_BG = "color(a98-rgb 0 0 0 / 1)"
+    GENERIC_MATCH = re.compile(RE_GENERIC_MATCH.format(color_space=SPACE))
 
     def __init__(self, color=DEF_BG):
         """Initialize."""
