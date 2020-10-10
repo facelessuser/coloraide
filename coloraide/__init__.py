dbaacbb354f4c3c69639b9cb06fafad0107abae5
facelessuser <faceless.shop@gmail.com>
2020-10-10 10:36:52 -0600

    Make CSS color object available from the main module

--- coloraide/__init__.py
+++ coloraide/__init__.py
@@ -1,2 +1,6 @@
 """ColorAide Library."""
 from .__meta__ import __version_info__, __version__  # noqa: F401
+from .css import Color
+from .colors import ColorMatch
+
+__all__ = ("Color", "ColorMatch")
