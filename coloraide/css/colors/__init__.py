0279714289eb09481cdccce568da443824c39c0e
facelessuser <faceless.shop@gmail.com>
2021-04-12 19:27:25 -0600

    Simplify CSS color overrides

--- coloraide/css/colors/__init__.py
+++ coloraide/css/colors/__init__.py
@@ -4,23 +4,14 @@ from .hsl import HSL
 from .hwb import HWB
 from .lab import LAB
 from .lch import LCH
-from ...colors import SRGBLinear
-from ...colors import HSV
-from ...colors import DisplayP3
-from ...colors import A98RGB
-from ...colors import ProPhotoRGB
-from ...colors import Rec2020
-from ...colors import XYZ
-from ...colors import XYZD65
 from ...colors import Color as GenericColor
 
-SUPPORTED = (
-    HSL, HWB, LAB, LCH, SRGB, SRGBLinear, HSV,
-    DisplayP3, A98RGB, ProPhotoRGB, Rec2020, XYZ, XYZD65
-)
+CSS_OVERRIDES = (HSL, HWB, LAB, LCH, SRGB)
 
 
 class Color(GenericColor):
     """Color wrapper class."""
 
-    CS_MAP = {obj.space(): obj for obj in SUPPORTED}
+    CS_MAP = {key: value for key, value in GenericColor.CS_MAP.items()}
+    for color in CSS_OVERRIDES:
+        CS_MAP[color.space()] = color
