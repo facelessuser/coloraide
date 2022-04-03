033f982e1c7b4475a30315b4665a3e0e5e1329de
facelessuser <faceless.shop@gmail.com>
2022-04-03 08:13:51 -0600

    Obtain extras wheel from our tag or master if not present
    This is mainly to ensure docs can build from tar.gz as in our CI,
    the wheel should already be present.

--- tools/buildwheel.py
+++ tools/buildwheel.py
@@ -3,21 +3,40 @@ import sys
 import subprocess
 import os
 import urllib.request
+import urllib.error
 import glob
 import shutil
 
 urls = [
     "https://files.pythonhosted.org/packages/6e/33/1ae0f71395e618d6140fbbc9587cc3156591f748226075e0f7d6f9176522/Markdown-3.3.4-py3-none-any.whl",  # noqa: E501
     "https://files.pythonhosted.org/packages/9a/9a/36f71797fbaf1f4b6cb8debe1ab6d3ec969e8dbc651181f131a16b794d80/pymdown_extensions-9.0-py3-none-any.whl",  # noqa: E501
-    # "https://files.pythonhosted.org/packages/9f/52/801913c6da8bb8f3657e8f15dafb450079f225d8564ada518bb545812417/coloraide_extras-0.2.0-py3-none-any.whl"  # noqa: E501
 ]
+
+
+def get_version():
+    """Get version and version_info without importing the entire module."""
+
+    import importlib.util
+
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'coloraide', '__meta__.py')
+    spec = importlib.util.spec_from_file_location("__meta__", path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    vi = module.__version_info__
+    return vi._get_canonical()
+
 
 # Keep a compatible version in the repository so we can always release docs.
 # If we ever have a breaking change, we can patch `coloraide-extras` before
 # the release, locally upload it and then make a release.
 # Afterwards, once `coloraide` is available, we can release the official
 # `coloraide-extras`. This prevents our documentation from ever breaking.
-keep = {'coloraide_extras-0.3.0-py3-none-any.whl'}
+EXTRA_VERSION = '0.3.0'
+EXTRA_WHEEL = 'coloraide_extras-{}-py3-none-any.whl'.format(EXTRA_VERSION)
+keep = {EXTRA_WHEEL}
+EXTRAS = "https://raw.githubusercontent.com/facelessuser/coloraide/{{}}/docs/src/markdown/playground/{}".format(
+    EXTRA_WHEEL
+)
 output = 'docs/src/markdown/playground/'
 externals = {}
 for url in urls:
@@ -52,13 +71,22 @@ def download_wheel(url, dest):
     """Download a wheel."""
 
     print('Downloading: {}'.format(url))
-    response = urllib.request.urlopen(url)
-    if response.status != 200:
-        print('Failed to download!')
-        return 1
-    with open(file, 'wb') as f:
-        print('Writing: {}'.format(dest))
-        f.write(response.read())
+    status = 0
+    try:
+        response = urllib.request.urlopen(url)
+        status = response.status
+        if status == 200:
+            status = 0
+            with open(dest, 'wb') as f:
+                print('Writing: {}'.format(dest))
+                f.write(response.read())
+    except urllib.error.HTTPError as e:
+        status = e.status
+
+    if status:
+        print('Failed to download, recieved status code {}'.format(status))
+
+    return status
 
 
 if __name__ == "__main__":
@@ -75,11 +103,38 @@ if __name__ == "__main__":
     if build_coloraide():
         sys.exit(1)
 
+    # Get dependencies
     for file, url in externals.items():
-        if os.path.basename(file) in keep and os.path.exists(file):
+        if os.path.exists(file):
             print('Skipping: {}'.format(file))
             continue
-        if download_wheel(url, file):
-            sys.exit(1)
+        status = download_wheel(url, file)
+        if status:
+            sys.exit(status)
 
-    sys.exit(0)
+    # Get coloraide-extras. If we can't find it in the version's tag,
+    # tag may not be created with this version, particularly during development,
+    # attempt on master
+    extras = EXTRAS.format(get_version())
+    extra_output = os.path.join(output, extras.split('/')[-1])
+    if not os.path.exists(extra_output):
+        status = download_wheel(extras, extra_output)
+        if status:
+            print(
+                (
+                    'Could not find ColorAide Extras at tag {},'
+                    'tag may not have been created yet, try to grab latest on master'
+                ).format(get_version())
+            )
+            extras = EXTRAS.format('master')
+            extra_output = os.path.join(output, extras.split('/')[-1])
+            status = download_wheel(extras, extra_output)
+            if status:
+                print('No available ColorAide Extras on master')
+    else:
+        status = 0
+        print('Skipping: {}'.format(extra_output))
+
+    print("FAILED :(" if status else "SUCCESS :)")
+
+    sys.exit(status)
