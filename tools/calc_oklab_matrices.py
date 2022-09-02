452401446051bce51b5b68e100401ce88e547441
facelessuser <faceless.shop@gmail.com>
2022-09-02 16:53:39 -0600

    Update Oklab matrices

--- tools/calc_oklab_matrices.py
+++ tools/calc_oklab_matrices.py
@@ -3,21 +3,22 @@ Calculate `oklab` matrices.
 
 https://github.com/w3c/csswg-drafts/issues/6642#issuecomment-945714988
 """
+from fractions import Fraction
 import sys
 import os
 
 sys.path.insert(0, os.getcwd())
 
+import tools.falgebra as alg  # noqa: E402
 import tools.calc_xyz_transform as xyzt  # noqa: E402
-from coloraide import algebra as alg  # noqa: E402
 
 # Calculated using our own `calc_xyz_transform.py`
 RGB_TO_XYZ, XYZ_TO_RGB = xyzt.get_matrix(xyzt.white_d65, 'srgb')
 
 M0 = [
-    [0.77849780, 0.34399940, -0.12249720],
-    [0.03303601, 0.93076195, 0.03620204],
-    [0.05092917, 0.27933344, 0.66973739]
+    [Fraction(77849780, 100000000), Fraction(34399940, 100000000), Fraction(-12249720, 100000000)],
+    [Fraction(3303601, 100000000), Fraction(93076195, 100000000), Fraction(3620204, 100000000)],
+    [Fraction(5092917, 100000000), Fraction(27933344, 100000000), Fraction(66973739, 100000000)]
 ]
 
 XYZ_TO_LMS = alg.divide(M0, alg.outer(alg.dot(M0, xyzt.white_d65), alg.ones(3)))
@@ -28,9 +29,9 @@ LMS_TO_SRGBL = alg.inv(SRGBL_TO_LMS)
 
 
 LMS3_TO_OKLAB = [
-    [0.2104542553, 0.793617785, -0.0040720468],
-    [1.9779984951, -2.428592205, 0.4505937099],
-    [0.0259040371, 0.7827717662, -0.808675766]
+    [Fraction(2104542553, 10000000000), Fraction(7936177850, 10000000000), Fraction(-40720468, 10000000000)],
+    [Fraction(19779984951, 10000000000), Fraction(-24285922050, 10000000000), Fraction(4505937099, 10000000000)],
+    [Fraction(259040371, 10000000000), Fraction(7827717662, 10000000000), Fraction(-8086757660, 10000000000)]
 ]
 
 OKLAB_TO_LMS3 = alg.inv(LMS3_TO_OKLAB)
@@ -45,7 +46,10 @@ def pprint(value):
             first = False
         else:
             print(',\n ', end='')
-        print(v, end='')
+        if isinstance(v, Fraction):
+            print(v.numerator / v.denominator, end='')
+        else:
+            print([c.numerator / c.denominator if isinstance(c, Fraction) else c for c in v], end='')
     print(']')
 
 
