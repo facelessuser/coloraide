2e28a9c7b301de387e91c2faa71ea9554d4b03c1
facelessuser <faceless.shop@gmail.com>
2023-03-23 07:35:01 -0600

    Remove redundant code
    LCh colors share the same Cartesian to polar translations

--- coloraide/spaces/lchuv.py
+++ coloraide/spaces/lchuv.py
@@ -4,32 +4,6 @@ from ..spaces import Space
 from ..cat import WHITES
 from ..channels import Channel, FLG_ANGLE
 from .lch import LCh
-from .. import util
-import math
-from ..types import Vector
-
-
-def luv_to_lchuv(luv: Vector) -> Vector:
-    """Luv to LChuv."""
-
-    l, u, v = luv
-
-    c = math.sqrt(u ** 2 + v ** 2)
-    h = math.degrees(math.atan2(v, u))
-
-    return [l, c, util.constrain_hue(h)]
-
-
-def lchuv_to_luv(lchuv: Vector) -> Vector:
-    """LChuv to Luv."""
-
-    l, c, h = lchuv
-
-    return [
-        l,
-        c * math.cos(math.radians(h)),
-        c * math.sin(math.radians(h))
-    ]
 
 
 class LChuv(LCh, Space):
@@ -44,13 +18,3 @@ class LChuv(LCh, Space):
         Channel("c", 0.0, 220.0, limit=(0.0, None)),
         Channel("h", 0.0, 360.0, flags=FLG_ANGLE)
     )
-
-    def to_base(self, coords: Vector) -> Vector:
-        """To Luv from LChuv."""
-
-        return lchuv_to_luv(coords)
-
-    def from_base(self, coords: Vector) -> Vector:
-        """From Luv to LChuv."""
-
-        return luv_to_lchuv(coords)
