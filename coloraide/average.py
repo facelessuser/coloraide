4e3270c2a2f33daf30465a6529b4d4da711b4021
facelessuser <faceless.shop@gmail.com>
2023-05-03 22:35:47 -0600

    Speed up averaging
    - Do premultiplication while summing channels
    - Undo premultiplication while calculating the mean
    - Don't use enumerate in critical loop

--- coloraide/average.py
+++ coloraide/average.py
@@ -1,41 +1,11 @@
 """Average colors together."""
 from __future__ import annotations
 import math
-from .types import ColorInput, Vector
+from .types import ColorInput
 from typing import Iterable, TYPE_CHECKING
 
 if TYPE_CHECKING:  # pragma: no cover
     from .color import Color
-
-
-def premultiply(coords: Vector, hue_index: int) -> Vector:
-    """Premultiply the color before averaging."""
-
-    alpha = coords[-1]
-
-    if math.isnan(alpha) or alpha == 1.0:
-        return coords
-
-    for i in range(len(coords) - 1):
-        if i != hue_index:
-            coords[i] *= alpha
-
-    return coords
-
-
-def postdivide(coords: Vector, hue_index: int) -> Vector:
-    """Undo premultiplication of semi-transparent colors."""
-
-    alpha = coords[-1]
-
-    if math.isnan(alpha) or alpha in (0.0, 1.0):
-        return coords
-
-    for i in range(len(coords) - 1):
-        if i != hue_index:
-            coords[i] /= alpha
-
-    return coords
 
 
 def average(create: type[Color], colors: Iterable[ColorInput], space: str, premultiplied: bool = True) -> Color:
@@ -48,41 +18,49 @@ def average(create: type[Color], colors: Iterable[ColorInput], space: str, premu
     hue_index = cs.hue_index() if hasattr(cs, 'hue_index') else -1
     channels = cs.channels
     chan_count = len(channels)
+    alpha_index = chan_count - 1
     sums = [0.0] * chan_count
     totals = [0.0] * chan_count
     sin = 0.0
     cos = 0.0
 
     # Sum channel values
-    e = -1
-    for e, c in enumerate(colors):
-        coords = premultiply(obj.update(c)[:], hue_index) if premultiplied else obj.update(c)[:]
-        for e, coord in enumerate(coords):
-            if math.isnan(coord):
-                continue
-            totals[e] += 1
-            if e == hue_index:
-                rad = math.radians(coord)
-                sin += math.sin(rad)
-                cos += math.cos(rad)
-            else:
-                sums[e] += coord
+    i = -1
+    for c in colors:
+        coords = obj.update(c)[:]
+        alpha = coords[-1]
+        if math.isnan(alpha):
+            alpha = 1.0
+        i = 0
+        for coord in coords:
+            if not math.isnan(coord):
+                totals[i] += 1
+                if i == hue_index:
+                    rad = math.radians(coord)
+                    sin += math.sin(rad)
+                    cos += math.cos(rad)
+                else:
+                    sums[i] += (coord * alpha) if premultiplied and i != alpha_index else coord
+            i += 1
 
-    if e == -1:
+    if i == -1:
         raise ValueError('At least one color must be provided in order to average colors')
 
     # Get the mean
-    for e, total in enumerate(totals):
+    alpha = sums[-1]
+    alpha_t = totals[-1]
+    sums[-1] = float('nan') if not alpha_t else alpha / alpha_t
+    alpha = sums[-1]
+    if math.isnan(alpha) or alpha in (0.0, 1.0):
+        alpha = 1.0
+    for i in range(chan_count - 1):
+        total = totals[i]
         if not total:
-            sums[e] = float('nan')
-        elif e == hue_index:
-            sums[e] = math.degrees(math.atan2(sin / total, cos / total))
+            sums[i] = float('nan')
+        elif i == hue_index:
+            sums[i] = math.degrees(math.atan2(sin / total, cos / total))
         else:
-            sums[e] /= total
-
-    # Undo premultiplication
-    if premultiplied:
-        sums = postdivide(sums, hue_index)
+            sums[i] /= total * alpha if premultiplied else total
 
     # Return the color
     return obj.update(space, sums[:-1], sums[-1])
