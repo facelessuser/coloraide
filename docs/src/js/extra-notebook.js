d54c1b651ce63630bab5196cc0a4b08a5804be4d
facelessuser <faceless.shop@gmail.com>
2023-05-23 06:33:12 -0600

    Update documentation dependencies

--- docs/src/js/extra-notebook.js
+++ docs/src/js/extra-notebook.js
@@ -123,7 +123,7 @@ ${content}
     if (!initialized) {
       initialized = true
       pyodide = await loadPyodide({ // eslint-disable-line no-undef
-        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.23.1/full/",
+        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.23.2/full/",
         fullStdLib: false
       })
     }
