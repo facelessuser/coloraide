a8683a21ed833a0c95db4a789369921299a8f8cd
facelessuser <faceless.shop@gmail.com>
2023-06-04 11:43:46 -0600

    Add more tests

--- tests/test_cct.py
+++ tests/test_cct.py
@@ -1,6 +1,9 @@
 """Test CCT."""
 import math
 from coloraide.everything import ColorAll as Color
+from coloraide import cmfs
+from coloraide import temperature
+from coloraide import cat
 from . import util
 import pytest
 import unittest
@@ -9,7 +12,7 @@ import unittest
 class TestOhno2013Temp(util.ColorAssertsPyTest):
     """Test Ohno 2013."""
 
-    @pytest.mark.parametrize('cct', list(range(1000, 100000, 50)))
+    @pytest.mark.parametrize('cct', list(range(1000, 100000, 100)))
     def test_cct(self, cct):
         """Test CCT methods."""
 
@@ -17,6 +20,19 @@ class TestOhno2013Temp(util.ColorAssertsPyTest):
             cct2, duv2 = Color.blackbody(cct, duv, space=None).cct()
             assert math.isclose(cct2, cct, rel_tol=(0.00001 if cct < 96000 else 0.0001), abs_tol=0.000001)
             assert math.isclose(duv2, duv, rel_tol=0.000001, abs_tol=0.000001)
+
+
+class TestRobertson1968(util.ColorAssertsPyTest):
+    """Test Ohno 2013."""
+
+    @pytest.mark.parametrize('cct', list(range(1000, 100000, 100)))
+    def test_cct(self, cct):
+        """Test CCT methods."""
+
+        for duv in (-0.03, -0.02, -0.01, 0.0, 0.01, 0.02, 0.03):
+            cct2, duv2 = Color.blackbody(cct, duv, space=None, method='robertson-1968').cct(method='robertson-1968')
+            assert math.isclose(cct2, cct, rel_tol=0.0001, abs_tol=0.00001)
+            assert math.isclose(duv2, duv, rel_tol=0.00001, abs_tol=0.00001)
 
 
 class TestOhno2013Color(util.ColorAssertsPyTest):
@@ -104,3 +120,19 @@ class TestCCTSpecificCases(util.ColorAsserts, unittest.TestCase):
             Color.blackbody(2000, space=None),
             Color('color(xyz-d65 1.2743 1 0.14524)')
         )
+
+    def test_ohno_alternate_cmfs(self):
+        """Test alternate CMFs."""
+
+        bbody = temperature.BlackBodyCurve(cmfs=cmfs.cie_1964_10deg, white=cat.WHITES['10deg']['D65'])
+        srgbl = Color.blackbody(5000, blackbody=bbody)
+        self.assertColorEqual(srgbl, Color('color(srgb-linear 1 0.77909 0.61807)'))
+        self.assertEqual(srgbl.cct(blackbody=bbody), [5000.005435889316, 2.770957661628331e-08])
+
+    def test_ohno_exact(self):
+        """Test alternate CMFs."""
+
+        cct1 = Color('orange').cct()
+        self.assertEqual(cct1, [2424.0755046224285, 0.008069417819174918])
+        cct2 = Color('orange').cct(exact=True)
+        self.assertEqual(cct2, [2424.0755046224285, 0.008069417819174918])
