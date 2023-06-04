18f39437a46a9c64dcbee94172f1f586cd1c5fff
facelessuser <faceless.shop@gmail.com>
2023-06-03 21:19:38 -0600

    Test to verify accuracy and adjust algorithm to better meet expectations

--- tests/test_cct.py
+++ tests/test_cct.py
@@ -6,7 +6,20 @@ import pytest
 import unittest
 
 
-class TestOhno2013(util.ColorAssertsPyTest):
+class TestOhno2013Temp(util.ColorAssertsPyTest):
+    """Test Ohno 2013."""
+
+    @pytest.mark.parametrize('cct', list(range(1000, 100000, 50)))
+    def test_cct(self, cct):
+        """Test CCT methods."""
+
+        for duv in (-0.03, -0.02, -0.01, 0.0, 0.01, 0.02, 0.03):
+            cct2, duv2 = Color.blackbody(cct, duv, space=None).cct()
+            assert math.isclose(cct2, cct, rel_tol=(0.00001 if cct < 96000 else 0.0001), abs_tol=0.000001)
+            assert math.isclose(duv2, duv, rel_tol=0.000001, abs_tol=0.000001)
+
+
+class TestOhno2013Color(util.ColorAssertsPyTest):
     """Test Ohno 2013."""
 
     @pytest.mark.parametrize(
@@ -31,7 +44,7 @@ class TestOhno2013(util.ColorAssertsPyTest):
         assert math.isclose(duv, duv2, abs_tol=0.05)
 
 
-class TestRobertson1968(util.ColorAssertsPyTest):
+class TestRobertson1968Color(util.ColorAssertsPyTest):
     """Test Robertson 1968."""
 
     @pytest.mark.parametrize(
