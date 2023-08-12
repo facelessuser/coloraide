12f4a324e1e4837c8aa39c573d1bed76ef20030e
facelessuser <faceless.shop@gmail.com>
2023-08-11 22:25:24 -0600

    Drop Python 3.7 support

--- hatch_build.py
+++ hatch_build.py
@@ -29,7 +29,6 @@ class CustomMetadataHook(MetadataHookInterface):
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
