66b1b8c930e07eb20a10b25fad61291354697094
facelessuser <faceless.shop@gmail.com>
2023-09-08 22:24:27 -0600

    Support Python 3.12

--- hatch_build.py
+++ hatch_build.py
@@ -33,6 +33,7 @@ class CustomMetadataHook(MetadataHookInterface):
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
             "Topic :: Software Development :: Libraries :: Python Modules",
             "Topic :: Text Processing :: Filters",
