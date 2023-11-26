ca1fbe07922a7c02326173f34c82581a98c35da0
facelessuser <faceless.shop@gmail.com>
2023-11-26 14:49:07 -0700

    Fix fill_diagonal types
    Types must be mutable

--- coloraide/algebra.py
+++ coloraide/algebra.py
@@ -2468,7 +2468,7 @@ def shape(a: ArrayLike | float) -> Shape:
     return _shape(a, ())
 
 
-def fill_diagonal(matrix: MatrixLike, val: float | ArrayLike, wrap: bool = False) -> None:
+def fill_diagonal(matrix: Matrix | Tensor, val: float | ArrayLike, wrap: bool = False) -> None:
     """Fill an N-D matrix diagonal."""
 
     s = shape(matrix)
