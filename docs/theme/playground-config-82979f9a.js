89f4fedfa7247bdd224494e09f6a114a94df4cd2
facelessuser <faceless.shop@gmail.com>
2024-03-02 08:59:24 -0700

    Update version number

---
+++ docs/theme/playground-config-82979f9a.js
@@ -0,0 +1,5 @@
+var colorNotebook = {
+    "playgroundWheels": ['Pygments-2.16.1-py3-none-any.whl', 'coloraide-3.1.1-py3-none-any.whl'],
+    "notebookWheels": ['pyyaml', 'Markdown-3.5.1-py3-none-any.whl', 'pymdown_extensions-10.5-py3-none-any.whl', 'Pygments-2.16.1-py3-none-any.whl', 'coloraide-3.1.1-py3-none-any.whl'],
+    "defaultPlayground": "import coloraide\ncoloraide.__version__\nColor('red')"
+}
