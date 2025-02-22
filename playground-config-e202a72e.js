cf6a3a992bfbdc04919992a53538bb68ecc87962
facelessuser <faceless.shop@gmail.com>
2025-02-22 16:25:52 +0000

    Deployed 007f9b04 with MkDocs version: 1.6.1

--- playground-config-e202a72e.js
+++
@@ -1,5 +0,0 @@
-var colorNotebook = {
-    "playgroundWheels": ['pygments-2.19.1-py3-none-any.whl', 'coloraide-4.2.1-py3-none-any.whl'],
-    "notebookWheels": ['pyyaml', 'Markdown-3.7-py3-none-any.whl', 'pymdown_extensions-10.14.1-py3-none-any.whl', 'pygments-2.19.1-py3-none-any.whl', 'coloraide-4.2.1-py3-none-any.whl'],
-    "defaultPlayground": "import coloraide\ncoloraide.__version__\nColor('red')"
-}
---
+++ playground-config-f798db25.js
@@ -0,0 +1,5 @@
+var colorNotebook = {
+    "playgroundWheels": ['micropip', 'pygments-2.19.1-py3-none-any.whl', 'coloraide-4.2.2-py3-none-any.whl'],
+    "notebookWheels": ['pyyaml', 'Markdown-3.7-py3-none-any.whl', 'pymdown_extensions-10.14.1-py3-none-any.whl', 'micropip', 'pygments-2.19.1-py3-none-any.whl', 'coloraide-4.2.2-py3-none-any.whl'],
+    "defaultPlayground": "import coloraide\ncoloraide.__version__\nColor('red')"
+}
