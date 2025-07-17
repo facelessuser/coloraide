238cce39239d9c12486fdbd939a1af6d1dcb3089
facelessuser <faceless.shop@gmail.com>
2025-07-17 15:17:56 -0600

    Rework some format handling for digital vs floating point

--- coloraide/spaces/sycc.py
+++ coloraide/spaces/sycc.py
@@ -6,23 +6,23 @@ https://www.color.org/sycc.pdf
 from __future__ import annotations
 from .ycbcr_709 import YCbCr, Environment
 from ..channels import Channel
-from ..spaces import Prism
+from ..spaces import Labish
 from ..cat import WHITES
 
 BT601 = [0.2990, 0.1140]
 
 
-class sYCC(Prism, YCbCr):
+class sYCC(Labish, YCbCr):
     """Y'CbCr color class using sRGB and BT.601 transform (8 bit)."""
 
     BASE = 'srgb'
     NAME = "sycc"
     SERIALIZE = ("--sycc",)
     WHITE = WHITES['2deg']['D65']
-    ENV = Environment(kr=BT601[0], kb=BT601[1], output='full', bit_depth=8)
+    ENV = Environment(kr=BT601[0], kb=BT601[1], standard=False, bit_depth=8)
     CHANNELS = (
-        Channel("y", ENV.y_range[0], ENV.y_range[1], bound=True),
-        Channel("cb", ENV.c_range[0], ENV.c_range[1], bound=True),
-        Channel("cr", ENV.c_range[0], ENV.c_range[1], bound=True)
+        Channel("y", ENV.y_range[0], ENV.y_range[1], nans=ENV.y_range[0], bound=True),
+        Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True),
+        Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True)
     )
     GAMUT_CHECK = 'srgb'
--- coloraide/spaces/ycbcr_2020.py
+++ coloraide/spaces/ycbcr_2020.py
@@ -19,10 +19,10 @@ class YCbCr2020(Prism, YCbCr):
     NAME = "ycbcr-2020"
     SERIALIZE = ("--ycbcr-2020",)
     WHITE = WHITES['2deg']['D65']
-    ENV = Environment(kr=BT2020[0], kb=BT2020[1], bit_depth=10)
+    ENV = Environment(kr=BT2020[0], kb=BT2020[1], bit_depth=10, standard=True)
     CHANNELS = (
-        Channel("y", ENV.y_range[0], ENV.y_range[1], bound=True),
-        Channel("cb", ENV.c_range[0], ENV.c_range[1], bound=True),
-        Channel("cr", ENV.c_range[0], ENV.c_range[1], bound=True)
+        Channel("y", ENV.y_range[0], ENV.y_range[1], nans=ENV.y_range[0], bound=True),
+        Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True),
+        Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True)
     )
     GAMUT_CHECK = 'rec2020'
