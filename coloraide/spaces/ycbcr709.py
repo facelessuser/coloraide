1495f519a9c589a4a42cd8df739a510a370c1b83
facelessuser <faceless.shop@gmail.com>
2025-07-18 10:13:42 -0600

    Provide YPbPr and YCbCr spaces and adjust logic, etc.

--- coloraide/spaces/ycbcr709.py
+++ coloraide/spaces/ycbcr709.py
@@ -18,16 +18,24 @@ from .. import util
 from ..channels import Channel
 from ..types import Vector
 from ..spaces import Luminant, Prism, Space
+from .lab import Lab
 from ..cat import WHITES
 from .. import algebra as alg
+from typing import Any, Callable
 
-BT709 = [0.2126, 0.0722]
+BT709 = (0.2126, 0.0722)
 
 
-def digital_round(x: float, env: Environment) -> float:
+def _digital_round(x: float, env: Environment) -> int:
     """Rounding for digital values."""
 
-    return alg.clamp(alg.round_half_up(x), 0, env.max_integer_size)
+    return alg.clamp(int(alg.round_half_up(x)), 0, env.max_integer_size)
+
+
+def digital_round(env: Environment) -> Callable[[float], int]:
+    """Round to integer."""
+
+    return lambda x, e=env: _digital_round(x, e)  # type: ignore[misc]
 
 
 class Environment:
@@ -36,9 +44,6 @@ class Environment:
     def __init__(
         self,
         *,
-        kr: float,
-        kb: float,
-        integer: bool = False,
         standard: bool = False,
         bit_depth: int = 8
     ) -> None:
@@ -46,16 +51,6 @@ class Environment:
 
         self.max_integer_size = (1 << bit_depth) - 1
         self.standard = standard
-        self.integer = integer
-
-        # Construct the Y'CbCr matrix
-        kg = 1 - kr - kb
-        self.rgb_to_ycbcr = [
-            [kr, kg, kb],
-            [-kr / (2 * (1 - kb)), -kg / (2 * (1 - kb)), 0.5],
-            [0.5, -kg / (2 * (1 - kr)), -kb / (2 * (1 - kr))]
-        ]
-        self.ycbcr_to_rgb = alg.inv(self.rgb_to_ycbcr)
 
         # Standard form which removes negative values and adds headroom/footroom
         if standard:
@@ -67,32 +62,61 @@ class Environment:
         # Removes negative values but extends values to full range without adding headroom/footroom
         # The default form cannot be in unsigned integer form and must be shifted
         else:
-            if integer:
-                self.y_scale = self.max_integer_size
-                self.y_offset = 0
-                self.c_scale = self.max_integer_size
-                self.c_offset = 1 << (bit_depth - 1)
-
-            # Floating point should revert to normal
-            else:
-                self.y_scale = self.max_integer_size
-                self.y_offset = 0
-                self.c_scale = self.max_integer_size
-                self.c_offset = 0
-
-        # Scale integer range down to 0 - 1
-        if not integer:
-            self.y_scale /= self.max_integer_size
-            self.y_offset /= self.max_integer_size
-            self.c_scale /= self.max_integer_size
-            self.c_offset /= self.max_integer_size
+            self.y_scale = self.max_integer_size
+            self.y_offset = 0
+            self.c_scale = self.max_integer_size
+            self.c_offset = 1 << (bit_depth - 1)
 
         # Calculate minimum and maximum ranges for color channels
         self.y_range = [self.y_offset + 0 * self.y_scale, self.y_offset + 1 * self.y_scale]
         self.c_range = [self.c_offset + -0.5 * self.c_scale, self.c_offset + 0.5 * self.c_scale]
 
 
-class YCbCr(Luminant, Space):
+class YPbPr(Lab):
+
+    ENV: Environment
+    CHANNELS = (
+        Channel("y", 0.0, 1.0, bound=True),
+        Channel("cb", -0.5, 0.5, bound=True),
+        Channel("cr", -0.5, 0.5, bound=True)
+    )
+    CHANNEL_ALIASES = {
+        'lightness': 'y'
+    }
+    K = (0.0, 0.0)
+
+    def __init__(self, **kwargs: Any) -> None:
+        """Initialize."""
+
+        super().__init__(**kwargs)
+
+        # Construct the transformation matrix
+        kr, kb = self.K
+        kg = 1 - kr - kb
+        self.rgb_to_ycbcr = [
+            [kr, kg, kb],
+            [-kr / (2 * (1 - kb)), -kg / (2 * (1 - kb)), 0.5],
+            [0.5, -kg / (2 * (1 - kr)), -kb / (2 * (1 - kr))]
+        ]
+        self.ycbcr_to_rgb = alg.inv(self.rgb_to_ycbcr)
+
+    def lightness_name(self) -> str:
+        """Get lightness name."""
+
+        return "y"
+
+    def to_base(self, coords: Vector) -> Vector:
+        """To base from oRGB."""
+
+        return alg.matmul(self.ycbcr_to_rgb, coords, dims=alg.D2_D1)
+
+    def from_base(self, coords: Vector) -> Vector:
+        """From base to oRGB."""
+
+        return alg.matmul(self.rgb_to_ycbcr, coords, dims=alg.D2_D1)
+
+
+class YCbCr(Luminant, Prism, Space):
     """Y'CbCr color class."""
 
     ENV: Environment
@@ -110,27 +134,22 @@ class YCbCr(Luminant, Space):
         """Check if color is achromatic."""
 
         env = self.ENV
-        if env.standard or env.integer:
-            o = env.c_offset
-            s = env.c_scale
-            return alg.rect_to_polar((coords[1] - o) / s, (coords[2] - o) / s)[0] < util.ACHROMATIC_THRESHOLD_SM
-        else:
-            return alg.rect_to_polar(coords[1], coords[2])[0] < util.ACHROMATIC_THRESHOLD_SM
+        o = env.c_offset
+        s = env.c_scale
+        return alg.rect_to_polar((coords[1] - o) / s, (coords[2] - o) / s)[0] < util.ACHROMATIC_THRESHOLD_SM
 
     def to_base(self, coords: Vector) -> Vector:
         """To base from oRGB."""
 
         env = self.ENV
-        if env.integer:
-            coords = [digital_round(c, env) for c in coords]
+        coords = [_digital_round(c, env) for c in coords]
         co = env.c_offset
         cs = env.c_scale
-        coords = [
+        return [
             (coords[0] - env.y_offset) / env.y_scale,
             (coords[1] - co) / cs,
             (coords[2] - co) / cs,
         ]
-        return alg.matmul(env.ycbcr_to_rgb, coords, dims=alg.D2_D1)
 
     def from_base(self, coords: Vector) -> Vector:
         """From base to oRGB."""
@@ -138,28 +157,36 @@ class YCbCr(Luminant, Space):
         env = self.ENV
         co = env.c_offset
         cs = env.c_scale
-        coords = alg.matmul(env.rgb_to_ycbcr, coords, dims=alg.D2_D1)
         coords = [
             env.y_offset + coords[0] * env.y_scale,
             co + coords[1] * cs,
             co + coords[2] * cs,
         ]
-        if env.integer:
-            coords = [digital_round(c, env) for c in coords]
-        return coords
+        return [_digital_round(c, env) for c in coords]
 
 
-class YCbCr709(Prism, YCbCr):
-    """Y'CbCr color class using Rec. 709 (8 bit)."""
+class YPbPr709(YPbPr):
+    """YPbPr color class using Rec. 709."""
 
     BASE = 'rec709'
-    NAME = "ycbcr-709"
-    SERIALIZE = ("--ycbcr-709",)
+    NAME = "ypbpr709"
+    SERIALIZE = ("--ypbpr709",)
     WHITE = WHITES['2deg']['D65']
-    ENV = Environment(kr=BT709[0], kb=BT709[1], bit_depth=8, standard=True)
+    K = BT709
+    GAMUT_CHECK = 'rec709'
+
+
+class YCbCr709(YCbCr):
+    """YCbCr color class using Rec. 709 (8 bit)."""
+
+    BASE = 'ypbpr709'
+    NAME = "ycbcr709-8bit"
+    SERIALIZE = ("--ycbcr709-8bit",)
+    WHITE = WHITES['2deg']['D65']
+    ENV = Environment(bit_depth=8, standard=True)
     CHANNELS = (
-        Channel("y", ENV.y_range[0], ENV.y_range[1], nans=ENV.y_range[0], bound=True),
-        Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True),
-        Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True)
+        Channel("y", ENV.y_range[0], ENV.y_range[1], nans=ENV.y_range[0], bound=True, limit=digital_round(ENV)),
+        Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=digital_round(ENV)),
+        Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=digital_round(ENV))
     )
     GAMUT_CHECK = 'rec709'
