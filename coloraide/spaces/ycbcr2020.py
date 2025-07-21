cfb7f71d7b37925536233a33c375ab491b0b6a74
facelessuser <faceless.shop@gmail.com>
2025-07-21 13:06:09 -0600

    More changes

--- coloraide/spaces/ycbcr2020.py
+++ coloraide/spaces/ycbcr2020.py
@@ -1,10 +1,11 @@
 """
-Y'CbCr color space.
+YCbCr color space.
 
-https://www.itu.int/dms_pubrec/itu-r/rec/bt/R-REC-BT.2020-2-201510-I!!PDF-E.pdf
+Rec. 2020
+- https://www.itu.int/dms_pubrec/itu-r/rec/bt/R-REC-BT.2020-2-201510-I!!PDF-E.pdf
 """
 from __future__ import annotations
-from .ycbcr709 import YPbPr, YCbCr, Environment, digital_round
+from .ycbcr709 import YPbPr, YCbCr, Environment
 from ..channels import Channel
 from ..cat import WHITES
 
@@ -18,21 +19,37 @@ class YPbPr2020(YPbPr):
     NAME = "ypbpr2020"
     SERIALIZE = ("--ypbpr2020",)
     WHITE = WHITES['2deg']['D65']
-    K = BT2020
+    ENV = Environment(k=BT2020)
     GAMUT_CHECK = 'rec2020'
 
 
-class YCbCr2020(YCbCr):
-    """Y'CbCr color class using Rec. 2020 (12 bit)."""
+class YCbCr2020Bit10(YCbCr):
+    """Y'CbCr color class using Rec. 2020 (10 bit)."""
 
-    BASE = 'ypbpr2020'
+    BASE = 'rec2020'
     NAME = "ycbcr2020-10bit"
     SERIALIZE = ("--ycbcr2020-10bit",)
     WHITE = WHITES['2deg']['D65']
-    ENV = Environment(bit_depth=10, standard=True)
+    ENV = Environment(k=BT2020, bit_depth=10, standard=True)
     CHANNELS = (
-        Channel("y", ENV.y_range[0], ENV.y_range[1], nans=ENV.y_range[0], bound=True, limit=digital_round(ENV)),
-        Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=digital_round(ENV)),
-        Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=digital_round(ENV))
+        Channel("y", ENV.y_range[0], ENV.y_range[1], nans=ENV.y_range[0], bound=True, limit=ENV.digital_round),
+        Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round),
+        Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round)
+    )
+    GAMUT_CHECK = 'rec2020'
+
+
+class YCbCr2020Bit12(YCbCr):
+    """Y'CbCr color class using Rec. 2020 (12 bit)."""
+
+    BASE = 'rec2020'
+    NAME = "ycbcr2020-12bit"
+    SERIALIZE = ("--ycbcr2020-12bit",)
+    WHITE = WHITES['2deg']['D65']
+    ENV = Environment(k=BT2020, bit_depth=12, standard=True)
+    CHANNELS = (
+        Channel("y", ENV.y_range[0], ENV.y_range[1], nans=ENV.y_range[0], bound=True, limit=ENV.digital_round),
+        Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round),
+        Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round)
     )
     GAMUT_CHECK = 'rec2020'
