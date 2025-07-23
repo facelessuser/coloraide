1629e33fa54e7bc0707f1c72ef66d76d988ffdb5
facelessuser <faceless.shop@gmail.com>
2025-07-23 15:01:27 -0600

    Update round trip

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -24,7 +24,9 @@ class TestRoundTrip:
     # These spaces, since they are digital, don't have the resolution to have good round trip throughout sRGB
     Color.deregister('space:sycc-8bit')
     Color.deregister('space:ycbcr709-8bit')
+    Color.deregister('space:ycbcr709-10bit')
     Color.deregister('space:ycbcr2020-10bit')
+    Color.deregister('space:ycbcr2020-12bit')
 
     SPACES = dict.fromkeys(Color.CS_MAP, 6)
     # Not as accurate due to approximation back to CAM16
@@ -98,7 +100,9 @@ class TestAchromaticRoundTrip(TestRoundTrip):
     # These spaces, since they are digital, don't have the resolution to have good round trip throughout sRGB
     Color.deregister('space:sycc-8bit')
     Color.deregister('space:ycbcr709-8bit')
+    Color.deregister('space:ycbcr709-10bit')
     Color.deregister('space:ycbcr2020-10bit')
+    Color.deregister('space:ycbcr2020-12bit')
 
     SPACES = dict.fromkeys(Color.CS_MAP, 6)
 
