9efc0e4f830990a1442345a874432bdb351968f3
facelessuser <faceless.shop@gmail.com>
2025-08-28 12:22:51 -0600

    Add sUCS and use the same D65 white point as ITP

--- coloraide/everything.py
+++ coloraide/everything.py
@@ -33,6 +33,7 @@ from .spaces.cam16_ucs import CAM16UCS, CAM16LCD, CAM16SCD
 from .spaces.hellwig import HellwigJMh, HellwigHKJMh
 from .spaces.zcam import ZCAMJMh
 from .spaces.scam import sCAMJMh
+from .spaces.sucs import sUCS
 from .spaces.hct import HCT
 from .spaces.ucs import UCS
 from .spaces.rec709 import Rec709
@@ -109,6 +110,7 @@ ColorAll.register(
         Rec2020OETF(),
         Msh(),
         sCAMJMh(),
+        sUCS(),
 
         # Delta E
         DE99o(),
