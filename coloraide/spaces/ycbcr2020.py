55d14b9051ae585ef761a1f28fceecb1c99305eb
facelessuser <faceless.shop@gmail.com>
2025-09-08 09:34:57 -0600

    Use OETF variants for now

--- coloraide/spaces/ycbcr2020.py
+++ coloraide/spaces/ycbcr2020.py
@@ -15,18 +15,18 @@ BT2020 = (0.2627, 0.0593)
 class YPbPr2020(YPbPr):
     """YPbPr color class using Rec. 709."""
 
-    BASE = 'rec2020'
+    BASE = 'rec2020-oetf'
     NAME = "ypbpr2020"
     SERIALIZE = ("--ypbpr2020",)
     WHITE = WHITES['2deg']['D65']
     ENV = Environment(k=BT2020)
-    GAMUT_CHECK = 'rec2020'
+    GAMUT_CHECK = 'rec2020-oetf'
 
 
 class YCbCr2020Bit10(YCbCr):
     """Y'CbCr color class using Rec. 2020 (10 bit)."""
 
-    BASE = 'rec2020'
+    BASE = 'rec2020-oetf'
     NAME = "ycbcr2020-10bit"
     SERIALIZE = ("--ycbcr2020-10bit",)
     WHITE = WHITES['2deg']['D65']
@@ -36,13 +36,13 @@ class YCbCr2020Bit10(YCbCr):
         Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round),
         Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round)
     )
-    GAMUT_CHECK = 'rec2020'
+    GAMUT_CHECK = 'rec2020-oetf'
 
 
 class YCbCr2020Bit12(YCbCr):
     """Y'CbCr color class using Rec. 2020 (12 bit)."""
 
-    BASE = 'rec2020'
+    BASE = 'rec2020-oetf'
     NAME = "ycbcr2020-12bit"
     SERIALIZE = ("--ycbcr2020-12bit",)
     WHITE = WHITES['2deg']['D65']
@@ -52,4 +52,4 @@ class YCbCr2020Bit12(YCbCr):
         Channel("cb", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round),
         Channel("cr", ENV.c_range[0], ENV.c_range[1], nans=ENV.c_range[0], bound=True, limit=ENV.digital_round)
     )
-    GAMUT_CHECK = 'rec2020'
+    GAMUT_CHECK = 'rec2020-oetf'
