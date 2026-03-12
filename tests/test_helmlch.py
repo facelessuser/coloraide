fec94fe3acb7f146dffee3a778a625a026501d48
facelessuser <faceless.shop@gmail.com>
2026-03-12 08:37:55 -0600

    Add more tests

---
+++ tests/test_helmlch.py
@@ -0,0 +1,155 @@
+"""Test Helmlch library."""
+import unittest
+from . import util
+from coloraide.everything import ColorAll as Color
+from coloraide import NaN
+import pytest
+
+
+class TestHelmlch(util.ColorAssertsPyTest):
+    """Test Helmlch."""
+
+    COLORS = [
+        ('red', 'color(--helmlch 0.7867 0.56424 338.92)'),
+        ('orange', 'color(--helmlch 0.97103 0.57488 28.682)'),
+        ('yellow', 'color(--helmlch 1.0591 0.51561 75.35)'),
+        ('green', 'color(--helmlch 0.55229 0.51461 106.59)'),
+        ('blue', 'color(--helmlch 0.54113 0.89231 244.8)'),
+        ('indigo', 'color(--helmlch 0.42572 0.31934 258.71)'),
+        ('violet', 'color(--helmlch 0.95228 0.36532 275.7)'),
+        ('white', 'color(--helmlch 1.1436 0 337.35)'),
+        ('gray', 'color(--helmlch 0.73661 0.00001 0.53612)'),
+        ('black', 'color(--helmlch 0 0 none)'),
+        # Test color
+        ('color(--helmlch 1.0 0.5 270)', 'color(--helmlch 1 0.5 270)'),
+        ('color(--helmlch 1.0 0.5 270 / 0.5)', 'color(--helmlch 1 0.5 270 / 0.5)'),
+        ('color(--helmlch 50% 50% 180 / 50%)', 'color(--helmlch 0.57181 0.5 180 / 0.5)'),
+        ('color(--helmlch none none none / none)', 'color(--helmlch none none none / none)'),
+        # Test percent ranges
+        ('color(--helmlch 0% 0% 0)', 'color(--helmlch 0 0 none)'),
+        ('color(--helmlch 100% 100% 360)', 'color(--helmlch 1.1436 1 360)'),
+        ('color(--helmlch -100% -100% -360)', 'color(--helmlch -1.1436 -1 -360)')
+    ]
+
+    @pytest.mark.parametrize('color1,color2', COLORS)
+    def test_colors(self, color1, color2):
+        """Test colors."""
+
+        self.assertColorEqual(Color(color1).convert('helmlch'), Color(color2))
+
+
+class TestHelmlchSerialize(util.ColorAssertsPyTest):
+    """Test Helmlch serialization."""
+
+    COLORS = [
+        # Test color
+        ('color(--helmlch 0.75 0.5 50 / 0.5)', {}, 'color(--helmlch 0.75 0.5 50 / 0.5)'),
+        # Test alpha
+        ('color(--helmlch 0.75 0.5 50)', {'alpha': True}, 'color(--helmlch 0.75 0.5 50 / 1)'),
+        ('color(--helmlch 0.75 0.5 50 / 0.5)', {'alpha': False}, 'color(--helmlch 0.75 0.5 50)'),
+        # Test None
+        ('color(--helmlch none 0.5 50)', {}, 'color(--helmlch 0 0.5 50)'),
+        ('color(--helmlch none 0.5 50)', {'none': True}, 'color(--helmlch none 0.5 50)'),
+        # Test Fit (not bound)
+        ('color(--helmlch 0.75 0.50 50)', {}, 'color(--helmlch 0.75 0.5 50)'),
+        ('color(--helmlch 0.75 0.50 50)', {'fit': False}, 'color(--helmlch 0.75 0.5 50)')
+    ]
+
+    @pytest.mark.parametrize('color1,options,color2', COLORS)
+    def test_colors(self, color1, options, color2):
+        """Test colors."""
+
+        self.assertEqual(Color(color1).to_string(**options), color2)
+
+
+class TestHelmlchProperties(util.ColorAsserts, unittest.TestCase):
+    """Test Helmlch."""
+
+    def test_lightness(self):
+        """Test `lightness`."""
+
+        c = Color('color(--helmlch 90% 0.5 120 / 1)')
+        self.assertEqual(c['lightness'], 1.0292592032779202)
+        c['lightness'] = 0.3
+        self.assertEqual(c['lightness'], 0.3)
+
+    def test_chroma(self):
+        """Test `chroma`."""
+
+        c = Color('color(--helmlch 90% 0.5 120 / 1)')
+        self.assertEqual(c['chroma'], 0.5)
+        c['chroma'] = 0.2
+        self.assertEqual(c['chroma'], 0.2)
+
+    def test_hue(self):
+        """Test `hue`."""
+
+        c = Color('color(--helmlch 90% 0.5 120 / 1)')
+        self.assertEqual(c['hue'], 120)
+        c['hue'] = 110
+        self.assertEqual(c['hue'], 110)
+
+    def test_alpha(self):
+        """Test `alpha`."""
+
+        c = Color('color(--helmlch 90% 0.5 120 / 1)')
+        self.assertEqual(c['alpha'], 1)
+        c['alpha'] = 0.5
+        self.assertEqual(c['alpha'], 0.5)
+
+
+class TestNull(util.ColorAsserts, unittest.TestCase):
+    """Test Null cases."""
+
+    def test_null_input(self):
+        """Test null input."""
+
+        c = Color('helmlch', [0.75, 0.2, NaN], 1)
+        self.assertTrue(c.is_nan('hue'))
+
+    def test_none_input(self):
+        """Test `none` null."""
+
+        c = Color('color(--helmlch 0.75 0 none / 1)')
+        self.assertTrue(c.is_nan('hue'))
+
+    def test_near_zero_null(self):
+        """
+        Test very near zero null.
+
+        This is a fix up to help give more sane hues
+        when chroma is very close to zero.
+        """
+
+        c = Color('color(--helmlch 0.75 0.000000000009 120 / 1)').convert('helmgen').convert('helmlch')
+        self.assertTrue(c.is_nan('hue'))
+
+    def test_from_helmlab(self):
+        """Test null from Helmgen conversion."""
+
+        c1 = Color('color(--helmlab 90% 0 0)')
+        c2 = c1.convert('helmlch')
+        self.assertColorEqual(c2, Color('color(--helmlch 90% 0 0)'))
+        self.assertTrue(c2.is_nan('hue'))
+
+    def test_null_normalization_min_chroma(self):
+        """Test minimum saturation."""
+
+        c = Color('color(--helmlch 90% 0 120 / 1)').normalize()
+        self.assertTrue(c.is_nan('hue'))
+
+
+class TestsAchromatic(util.ColorAsserts, unittest.TestCase):
+    """Test achromatic."""
+
+    def test_achromatic(self):
+        """Test when color is achromatic."""
+
+        self.assertEqual(Color('helmlch', [0.3, 0, 270]).is_achromatic(), True)
+        self.assertEqual(Color('helmlch', [0.3, 0.00000001, 270]).is_achromatic(), True)
+        self.assertEqual(Color('helmlch', [NaN, 0.00000001, 270]).is_achromatic(), True)
+        self.assertEqual(Color('helmlch', [0, NaN, 270]).is_achromatic(), True)
+        self.assertEqual(Color('helmlch', [0, 0.4, 270]).is_achromatic(), False)
+        self.assertEqual(Color('helmlch', [NaN, 0.1, 270]).is_achromatic(), False)
+        self.assertEqual(Color('helmlch', [0.3, NaN, 270]).is_achromatic(), True)
+        self.assertEqual(Color('helmlch', [NaN, NaN, 270]).is_achromatic(), True)
