57c6bbc483a067f70b9cb2631dae72967f14342d
facelessuser <faceless.shop@gmail.com>
2026-06-25 18:58:04 -0600

    Bump version

--- docs/theme/playground-config-30155151.js
+++
@@ -1,5 +0,0 @@
-var colorNotebook = {
-    "playgroundWheels": ['micropip', 'pygments-2.20.0-py3-none-any.whl', 'coloraide-8.9-py3-none-any.whl'],
-    "notebookWheels": ['pyyaml', 'markdown-3.10.2-py3-none-any.whl', 'pymdown_extensions-10.21.3-py3-none-any.whl', 'micropip', 'pygments-2.20.0-py3-none-any.whl', 'coloraide-8.9-py3-none-any.whl'],
-    "defaultPlayground": "import coloraide\ncoloraide.__version__\nColor('red')"
-}
---
+++ docs/theme/playground-config-331fdad2.js
@@ -0,0 +1,5 @@
+var colorNotebook = {
+    "playgroundWheels": ['micropip', 'pygments-2.20.0-py3-none-any.whl', 'coloraide-8.10-py3-none-any.whl'],
+    "notebookWheels": ['pyyaml', 'markdown-3.10.2-py3-none-any.whl', 'pymdown_extensions-10.21.3-py3-none-any.whl', 'micropip', 'pygments-2.20.0-py3-none-any.whl', 'coloraide-8.10-py3-none-any.whl'],
+    "defaultPlayground": "import coloraide\ncoloraide.__version__\nColor('red')"
+}
